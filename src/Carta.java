9690c5afb3643c7b4bed097f6aad85946ef62917
andre-estacio <36708555+andre-estacio@users.noreply.github.com>
2018-09-15 10:44:52 -0300

    carta

--- src/Carta.java
+++ src/Carta.java
@@ -1,10 +1,13 @@
+package Trunfo;
+
 public class Carta {
     String nome;
     int paginas;
     int preco;
     int capitulos;
     int copiasVendidas;
-
+    
+    
     public Carta(String nome, int paginas, int preco, int capitulos, int copiasVendidas){
         this.nome = nome;
         this.paginas = paginas;
