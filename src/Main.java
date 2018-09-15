55a129e04f2e19579b308bcbc585deaed595c66b
andre-estacio <36708555+andre-estacio@users.noreply.github.com>
2018-09-15 10:50:14 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
-import java.io.File;
+package Trunfo;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Baralho b = new Baralho();
-        System.out.println(b.cartas.get(31).nome + "\n");
-
-        JogadorBot j = new JogadorBot();
-
-        int i = j.escolherCaracteristica();
-
-        System.out.println("Escolha: " + i);
+        
+        //Inicializa Jogadores
+        JogadorBot r = new JogadorBot();
+        Jogador j = new Jogador();
+        
+        //Distribuição de cartas
+        Jogo g = new Jogo(b , j, r);
+        g.distribuirCartas();
+          
+        //Mostra o baralho
+        int i;
+        System.out.println("Seu Baralho:");
+        for (i=0;i<12;i++) {
+        	System.out.println("Carta: "+j.monte.get(i).nome);
+        }        
+        	System.out.println(" ");
+        	System.out.println(" ");
+        	System.out.println(" ");
+        do {
+        	j.comprarCarta();
+        	r.comprarCarta();
+        	j.mostraCarta();
+        	g.entregaVencedor();
+        }while(j.monte.isEmpty() || r.monte.isEmpty());
     }
 }
