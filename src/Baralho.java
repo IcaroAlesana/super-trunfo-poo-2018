5263842e2ba3f3bb8758da03bdef3c4bc685a667
andre-estacio <36708555+andre-estacio@users.noreply.github.com>
2018-09-16 21:46:46 -0300

    Update Baralho.java

--- src/Baralho.java
+++ src/Baralho.java
@@ -9,17 +9,18 @@ import java.util.Scanner;
 public class Baralho {
     ArrayList<Carta> cartas;
 
-    //construtor inicializa o arraylist e lê o baralho a partir de um arquivo txt.
+    //construtor inicializa o arraylist e lÃª o baralho a partir de um arquivo txt.
     public Baralho(){
         Scanner s;
         this.cartas = new ArrayList<Carta>();
         try {
             s = new Scanner(new File("livros.txt"));
             for(int i = 0; i<32; i++){
-                this.cartas.add(new Carta(s.nextLine(), Integer.parseInt(s.nextLine()), Integer.parseInt(s.nextLine()), Integer.parseInt(s.nextLine()), Integer.parseInt(s.nextLine())));
+            	this.cartas.add(new Carta(s.nextLine(), Integer.parseInt(s.nextLine()), Integer.parseInt(s.nextLine()), Integer.parseInt(s.nextLine()), Integer.parseInt(s.nextLine()), s.nextLine()));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 }
+
