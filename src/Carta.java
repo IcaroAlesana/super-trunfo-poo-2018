79a32839313a17b40c58514e69ed2ce74006bea6
andre-estacio <36708555+andre-estacio@users.noreply.github.com>
2018-09-16 21:47:14 -0300

    Update Carta.java

--- src/Carta.java
+++ src/Carta.java
@@ -6,13 +6,14 @@ public class Carta {
     int preco;
     int capitulos;
     int copiasVendidas;
+    String codigo;
     
-    
-    public Carta(String nome, int paginas, int preco, int capitulos, int copiasVendidas){
+    public Carta(String nome, int paginas, int preco, int capitulos, int copiasVendidas, String codigo){
         this.nome = nome;
         this.paginas = paginas;
         this.preco = preco;
         this.capitulos = capitulos;
         this.copiasVendidas = copiasVendidas;
+        this.codigo = codigo;
     }
 }
