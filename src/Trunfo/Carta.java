f92e98df37c77bbea626588a645d44813c15ce97
Icaro Cristofolini <icaroaj@hotmail.com>
2018-09-17 19:01:00 -0300

    Correção de bugs

--- src/Trunfo/Carta.java
+++ src/Trunfo/Carta.java
@@ -20,6 +20,6 @@ public class Carta {
 
     public void imprimeCarta(){
 
-        System.out.println("Nome: " +this.nome + "\n" + "Preço: " + this.preco + "\n" + "Nº de Páginas: " + this.paginas + "\n" + "Nº de Capítulos: " + this.capitulos + "\n" + "Cópias vendidas: " + this.copiasVendidas + "\n" + "Código: " + this.codigo + "\n\n");
+        System.out.println("Nome: " +this.nome + "\n" + "Nº de páginas: " + this.paginas + "\n" + "Preço: " + this.preco + "\n" + "Nº de Capítulos: " + this.capitulos + "\n" + "Cópias vendidas: " + this.copiasVendidas + "\n" + "Código: " + this.codigo + "\n\n");
     }
 }
