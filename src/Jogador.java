fe2a7e2fccadb2940e201920e64a93fa8d3f7416
andre-estacio <36708555+andre-estacio@users.noreply.github.com>
2018-09-16 21:46:18 -0300

    Update Jogador.java

--- src/Jogador.java
+++ src/Jogador.java
@@ -13,23 +13,45 @@ public class Jogador {
     }
 
     void comprarCarta() {
-        Carta c = monte.get(0);
+        Carta c =  monte.get(0);
+        monte.remove(0);
         this.mao = c;
     }
 
+    void mostraBaralho() {
+    int i;
+  		System.out.println("");
+    	System.out.println("---------------");
+      	System.out.println("");
+    	System.out.println("Seu Baralho:");
+        System.out.println(" ");
+        for (i=0;i<this.monte.size();i++) {
+        	System.out.println("Carta "+ i+": "+monte.get(i).nome);
+        }
+    }
+
     void mostraCarta() {
+  		System.out.println("");
+    	System.out.println("---------------");
+      	System.out.println("");
     	System.out.println("A carta comprada tem as seguintes caracteristicas:");
-    	System.out.println("Título:"+this.mao.nome);
-    	System.out.println("Capitulos:"+this.mao.capitulos);
-    	System.out.println("Cópias Vendidas:"+this.mao.copiasVendidas);
-    	System.out.println("Páginas:"+this.mao.paginas);
-    	System.out.println("Preço:"+this.mao.preco);  	
+    	System.out.println("");
+    	System.out.println("Título:          "+this.mao.nome);
+       	System.out.println("Páginas:         "+this.mao.paginas);
+       	System.out.println("Preço:           "+this.mao.preco);  
+    	System.out.println("Capitulos:       "+this.mao.capitulos);
+    	System.out.println("Cópias Vendidas: "+this.mao.copiasVendidas);
+    	System.out.println("Código:          "+this.mao.codigo);
     }
     
-    int escolherCaracteristica(){
-        System.out.println("Digite o nÃºmero correspondente a caracterÃ­stica que deseja utilizar: \n1 - Paginas\n2 - PreÃ§o\n3 - CapÃ­tulos\n4 - CÃ³pias Vendidas\n");
-        s = new Scanner(System.in);
-        //Scanner s = new Scanner(System.in);
+    int escolherCaracteristica(){ 
+      	System.out.println("");
+    	System.out.println("---------------");
+      	System.out.println("");
+        System.out.println("Digite o nÃºmero correspondente a caracterÃ­stica que deseja utilizar:");
+        System.out.println("");
+        System.out.println("1 - Paginas \n2 - PreÃ§o\n3 - CapÃ­tulos\n4 - CÃ³pias Vendidas\n");
+        Scanner s = new Scanner(System.in);
         int escolha = s.nextInt();
         return escolha;
     }
